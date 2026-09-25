ab5a416780f640a9b2de7436fada71d5bae3bd68
joaotech12 <zeroo01010010@gmail.com>
2026-09-24 21:47:06 -0300

    tela inicial, falta css

--- script.js
+++ script.js
@@ -12,3 +12,39 @@ if(!user){
 
 }
 verificarLogin()
+
+let paginaAtual = 0 
+async function artApi() {
+    const resposta = await fetch(`https://api.artic.edu/api/v1/artworks?page=${paginaAtual + 1}&fields=id,title,artist_display,image_id`)
+    const data = await resposta.json();
+    console.log(data);
+    return data.data;
+}
+
+async function postApi(){
+    const main = document.getElementById("feed")
+    const obras = await artApi();
+
+    obras.forEach(obra => {
+        if (!obra.image_id) return;
+        const imagemUrl = `https://www.artic.edu/iiif/2/${obra.image_id}/full/843,/0/default.jpg`
+
+        main.insertAdjacentHTML(`beforeend`, `
+            <div class="art slide" id="art${obra.id}">
+                <div class="artImagem">
+                    <img src="${imagemUrl}" alt="${obra.title}" loading="lazy" >
+                </div>
+                <div class="artInfo">
+                    <h2 class="artTitulo">${obra.title}</h2>
+                    <p class="artTitulo">${obra.artist_display}</p>
+                </div>
+            </div>
+        `);
+    });
+}
+
+postApi()
+
+
+
+
